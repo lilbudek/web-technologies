503cd1d14666189ac8007cb3700f2c05685d2c79
lilbudek <exemle.cf@mail.ru>
2023-04-15 00:48:14 +0700

    добавил систему комментариев к постам

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status='published')
@@ -23,11 +24,13 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default='draft')
-    objects = models.Manager() # Менеджер по умолчанию.
-    published = PublishedManager() # Наш новый менеджер.
+    objects = models.Manager()  # Менеджер по умолчанию.
+    published = PublishedManager()  # Наш новый менеджер.
+
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year,
-                        self.publish.month, self.publish.day, self.slug])
+                                                 self.publish.month, self.publish.day, self.slug])
+
 
 class Meta:
     ordering = ('-publish',)
@@ -35,3 +38,23 @@ class Meta:
 
 def __str__(self):
     return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+
+
+class Meta:
+    ordering = ('created',)
+
+
+def __str__(self):
+    return 'Comment by {} on {}'.format(self.name, self.post)
