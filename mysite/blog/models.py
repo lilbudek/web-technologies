b5b40be8c719d3d8509eb9daeb7c589cb14d9b10
lilbudek <exemle.cf@mail.ru>
2023-04-16 17:59:53 +0700

    добавил систему тегов

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -24,8 +25,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default='draft')
-    objects = models.Manager()  # Менеджер по умолчанию.
-    published = PublishedManager()  # Наш новый менеджер.
+    objects = models.Manager()  # менеджер по умолчанию
+    published = PublishedManager()  # наш новый менеджер
+    tags = TaggableManager() # менеджер тегов
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year,
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -13,8 +15,12 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)  # По 3 статьи на каждой странице.
     page = request.GET.get('page')
     try:
@@ -25,7 +31,7 @@ def post_list(request):
     except EmptyPage:
         # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -53,12 +59,19 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # Формирование списка похожих статей.
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
